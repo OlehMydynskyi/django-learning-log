7557fe354178ee5849809dde413a82d52923f3f0
OlehMydynskyi <mydynskyioleh@gmai.com>
2024-03-14 17:30:10 +0200

    added topic and topics pages

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_log/index.html')
+
+def topics(request):
+    """Shows all topics."""
+    topics = Topic.objects.order_by('date_added')
+    content = {'topics': topics}
+    return render(request, 'learning_log/topics.html', content)
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    content = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_log/topic.html', content)
